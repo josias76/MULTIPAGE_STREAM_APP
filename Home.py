b8e580eae114e9336db43bf1167ca556c29d5911
josias76 <josias76nteme@gmail.com>
2025-05-17 18:32:26 +0200

    good

--- Home.py
+++ Home.py
@@ -1,10 +1,35 @@
 import streamlit as st
 
 
+
+my_var = "This is a home variable"
+
+
+
+
+
+
+
 def main () : 
     st.header("HOME PAGE")
     st.title("APPLICATION MULTIPAGES")
+    st.write(my_var)
 
+
+    choix = st.sidebar.radio("SubMenu",["Description","Documentation"])
+    if choix == "Description" : 
+        st.subheader("Voici la description de l'application web")
+    if choix == "Documentation" : 
+        st.subheader("Voici la Documentation de l'application web")
 
 if __name__ == '__main__':
     main ()
+
+st.sidebar.image("logo1.jpeg", caption="SDA Consulting")
+# Bande de bas de page
+st.markdown("""
+    <hr style="border-top: 1px solid #4CAF50; margin-top: 50px;"/>
+    <div style="text-align: center; color: #888; font-size: 0.9em;">
+        &copy; 2025 <strong>Josias Nteme</strong> - Tous droits réservés.
+    </div>
+""", unsafe_allow_html=True)
---
+++ pages/02_analysis.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+
+st.subheader("Data Analysis Page ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+st.sidebar.image("logo1.jpeg", caption="SDA Consulting")
+# Bande de bas de page
+st.markdown("""
+    <hr style="border-top: 1px solid #4CAF50; margin-top: 50px;"/>
+    <div style="text-align: center; color: #888; font-size: 0.9em;">
+        &copy; 2025 <strong>Josias Nteme</strong> - Tous droits réservés.
+    </div>
+""", unsafe_allow_html=True)
